ecbbf9fadea1d17c34eb9ba86a0ed96f0c31ece1
Ashfaq3442 <ashfaqm46@gmail.com>
2024-10-16 02:45:32 +0500

    scrape one page successfully

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -4,7 +4,17 @@ import scrapy
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        books=response.css("article.product_pod"),
+
+        for book in books:
+            yield{
+                "name": book.css("h3 a::text").get(),
+                "price": book.css("div.product_price p::text").get(),
+                "url": book.css("h3 a").attrib["href"],
+            }
+
+    
+        
